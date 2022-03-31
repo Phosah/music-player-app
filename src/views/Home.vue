4b1a86bcce2473a95081d8f5593ad8a4fdf13ad2
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-03-31 07:12:10 -0700

    Update to database

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,21 +9,20 @@
           <div class>
             <Banner />
             <div>
-              <NewSongRelease :songs="songReleaseArr" />
-              <SongsYouMayLike :songs="songsYouMayLikeArr" />
-              <RecentlyPlayedSongs :recentlyPlayedSongs="playedSongsArr" />
+              <NewSongRelease :songs="newReleaseArray" />
+              <SongsYouMayLike :songs="songsYouMayLikeArray" />
+              <RecentlyPlayedSongs :recentlyPlayedSongs="recentlyPlayedSongsArr" />
             </div>
           </div>
         </div>
         <div class="w-2/6">
-          <TopStreams :streams="songsDb" />
+          <TopStreams :streams="topStreamsArray" />
           <Categories :categories="categoriesArr" />
         </div>
       </div>
     </div>
     <NowPlaying />
   </main>
-  <div>{{ songsDb }}</div>
 </template>
 
 <script lang="ts">
@@ -34,32 +33,12 @@ import NewSongRelease from "../components/NewSongRelease.vue";
 import type LatestSongRelease from "../types/LatestSongRelease";
 import TopStreams from "../components/TopStreams.vue";
 import type Streams from "@/types/TopStreams";
-import img1 from "@/assets/new-release-1.png";
-import img2 from "@/assets/new-release-2.png";
-import img3 from "@/assets/new-release-3.png";
-import img4 from "@/assets/new-release-4.png";
-import img5 from "@/assets/top-streams-1.png";
-import img6 from "@/assets/top-streams-2.png";
-import img7 from "@/assets/top-streams-3.png";
-import img8 from "@/assets/top-streams-4.png";
-import img9 from "@/assets/top-streams-5.png";
-import img10 from "@/assets/top-streams-6.png";
-import img11 from "@/assets/top-streams-7.png";
-import img12 from "@/assets/like-1.png"
-import img13 from "@/assets/like-2.png"
-import img14 from "@/assets/like-3.png"
-import img15 from "@/assets/like-4.png"
-import img16 from "@/assets/like-5.png"
-import img17 from "@/assets/like-6.png"
-import img18 from "@/assets/like-7.png"
 import pop from "@/assets/category-1.png";
 import jazz from "@/assets/category-2.png";
 import romance from "../assets/category-3.png";
 import chill from "@/assets/category-4.png";
 import hiphop from "@/assets/category-5.png";
 import rock from "@/assets/category-6.png";
-import img20 from "../assets/recently-played-1.png"
-import img21 from "../assets/recently-played-2.png"
 import Categories from "../components/Categories.vue";
 import type Category from "@/types/Categories";
 import SongsYouMayLike from "../components/SongsYouMayLike.vue";
@@ -75,96 +54,6 @@ export default defineComponent({
   components: { Sidebar, Banner, NewSongRelease, TopStreams, Categories, SongsYouMayLike, RecentlyPlayedSongs, NowPlaying },
 
   setup() {
-    const songReleaseArr = ref<LatestSongRelease[]>([
-      {
-        img: img1,
-        alt: "Taylor swift",
-        title: "Red (Taylor’s Version)",
-        artiste: "Taylor Swift"
-      },
-      {
-        img: img2,
-        alt: "Dojo Cat",
-        title: "Need to Know",
-        artiste: "Dojo Cat"
-      },
-      {
-        img: img3,
-        alt: "The weekend",
-        title: "After Hours",
-        artiste: "The Weekend"
-      },
-      {
-        img: img4,
-        alt: "Soundwave",
-        title: "Hit Machine",
-        artiste: "Soundwave"
-      },
-      {
-        img: img1,
-        alt: "Taylor swift",
-        title: "Red (Taylor’s Version)",
-        artiste: "Taylor Swift"
-      },
-    ])
-    const topStreamsArr = ref<Streams[]>([
-      {
-        id: 1,
-        img: img8,
-        alt: "Justin bieber",
-        title: "Mistletoe",
-        artiste: "Justin Bieber",
-        duration: "3:45",
-      },
-      {
-        id: 2,
-        img: img6,
-        alt: "Adele",
-        title: "Easy on me",
-        artiste: "Adele",
-        duration: "3:45",
-      },
-      {
-        id: 3,
-        img: img7,
-        alt: "Public Library Community",
-        title: "Moonlight",
-        artiste: "Public Library Commu...",
-        duration: "3:45",
-      },
-      {
-        id: 4,
-        img: img8,
-        alt: "Taylor swift",
-        title: "Red (Taylor’s Version)",
-        artiste: "Taylor Swift",
-        duration: "3:45",
-      },
-      {
-        id: 5,
-        img: img9,
-        alt: "88rising",
-        title: "Midsummer Madness",
-        artiste: "88rising",
-        duration: "3:45",
-      },
-      {
-        id: 6,
-        img: img10,
-        alt: "Pink Sweat$",
-        title: "I feel good",
-        artiste: "Pink Sweat$",
-        duration: "3:45",
-      },
-      {
-        id: 7,
-        img: img11,
-        alt: "Vincent Fable",
-        title: "Get lost",
-        artiste: "Vincent Fable",
-        duration: "3:45",
-      },
-    ])
     const categoriesArr = ref<Category[]>([
       {
         img: pop,
@@ -197,74 +86,13 @@ export default defineComponent({
         name: "Chill"
       },
     ])
-    const songsYouMayLikeArr = ref<SongsToLike[]>([
-      {
-        img: img12,
-        alt: "Shaun",
-        title: "Way Back Home",
-        artiste: "Shaun",
-      },
-      {
-        img: img18,
-        alt: "The Clean Bandit",
-        title: "Rockabye",
-        artiste: "The Clean Bandit",
-      },
-      {
-        img: img13,
-        alt: "Pink Sweat$",
-        title: "All my worst",
-        artiste: "Pink Sweat$",
-      },
-      {
-        img: img17,
-        alt: "Kanye West",
-        title: "Graduation",
-        artiste: "Kanye West",
-      },
-      {
-        img: img16,
-        alt: "Zedd",
-        title: "Stay",
-        artiste: "Zedd",
-      },
-      {
-        img: img15,
-        alt: "Gayle",
-        title: "abcdefu",
-        artiste: "Gayle",
-      },
-      {
-        img: img14,
-        alt: "Ed Sheeran",
-        title: "Bad Habits",
-        artiste: "Ed Sheeran",
-      },
 
-    ])
-    const playedSongsArr = ref<RecentlyPlayed[]>([
-      {
-        img: img20,
-        alt: "Maria Carey",
-        title: "All I Want For Christmas Is You",
-        artiste: "Maria Carey",
-        album: "Album",
-        duration: "3:45"
-      },
-      {
-        img: img21,
-        alt: "Rose",
-        title: "On The Ground",
-        artiste: "Rose",
-        album: "-R-",
-        duration: "3:45"
-      },
+    const topStreamsArray = ref<Streams[]>((AllSongs.topStreams))
+    const newReleaseArray = ref<LatestSongRelease[]>(AllSongs.newRelease)
+    const songsYouMayLikeArray = ref<SongsToLike[]>(AllSongs.songsYouMayLike)
+    const recentlyPlayedSongsArr = ref<RecentlyPlayed[]>(AllSongs.recentlyPlayed)
 
-    ])
-
-    const songsDb = ref<Streams[]>((AllSongs.songs))
-
-    return { songReleaseArr, topStreamsArr, categoriesArr, songsYouMayLikeArr, playedSongsArr, songsDb }
+    return { categoriesArr, topStreamsArray, newReleaseArray, songsYouMayLikeArray, recentlyPlayedSongsArr }
   }
 })
 </script>
